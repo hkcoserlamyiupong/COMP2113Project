1ea9d99bfc07afce5ee2fbff4ba4bc6fedf7bb5e
hkcoserlamyiupong <63230294+hkcoserlamyiupong@users.noreply.github.com>
2020-04-09 18:43:38 +0800

    Add files via upload

---
+++ drawcardwithdiamond.cpp
@@ -0,0 +1,11 @@
+//drawcardwithdiamond.cpp
+#include <iostream>
+#include <cstdlib>
+#include "drawcardwithdiamond.h"
+using namespace std;
+//draw card with diamond function
+int drawcardwithdiamond(int &diamond){
+  srand(time(NULL))
+  diamond-=5;
+  return rand()%10;//number of high quality roles
+}
