adb45f0be96da5fd455e8c3546a7c9a067c526b1
hkcoserlamyiupong <63230294+hkcoserlamyiupong@users.noreply.github.com>
2020-04-09 18:31:44 +0800

    Add files via upload

---
+++ drawcardwithheart.cpp
@@ -0,0 +1,11 @@
+//drawcardwithheart.cpp
+#include <iostream>
+#include <cstdlib>
+#include "drawcardwithheart.h"
+using namespace std;
+//draw card with diamond function
+int drawcardwithheart(&heart){
+  srand(time(NULL))
+  diamond-=5;
+  return rand()%10+10;//number of low quality roles, number of high quality roles
+}
