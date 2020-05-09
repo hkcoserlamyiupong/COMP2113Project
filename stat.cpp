d4686fc71e145b37db63e5d2e337bf523f5c99a2
hkcoserlamyiupong <63230294+hkcoserlamyiupong@users.noreply.github.com>
2020-05-09 21:49:26 +0800

    Add files via upload

--- stat.cpp
+++ stat.cpp
@@ -12,40 +12,40 @@ void stat(info team[], monsterinfo monster[], int t){
   ofstream fout;
   fout.open("stat.txt", ios::app);
   for (int i = 0; i < 5; i++){
-    cout<<"team member "<<i<<" "<<team[i].name<<endl;
+    fout<<"team member "<<i<<" "<<team[i].name<<endl;
     if (team[i].dead==true){
-      cout<<"dead"<<endl;
+      fout<<"dead"<<endl;
     }
     else {
-      cout<<"skill "<<team[i].skillname;
+      fout<<"skill "<<team[i].skillname;
       if (team[i].cd<=0){
-        cout<<" ready";
+        fout<<" ready";
       }
-      cout<<" cd:"<<team[i].cd<<"/"<<team[i].maxcd<<" hp:"<<team[i].hp<<"/"<<team[i].maxhp<<" position:("<<team[i].x<<","<<team[i].y<<")"<<" radiation:"<<team[i].radiation;
+      fout<<" cd:"<<team[i].cd<<"/"<<team[i].maxcd<<" hp:"<<team[i].hp<<"/"<<team[i].maxhp<<" position:("<<team[i].x<<","<<team[i].y<<")"<<" radiation:"<<team[i].radiation;
       if (team[i].ignited==true){
-        cout<<" ignited, effect reamins for"<<t-team[i].ignitedt;
+        fout<<" ignited, effect reamins for"<<t-team[i].ignitedt;
       }
       if (team[i].occupy>0){
-        cout<<" occupying computer for"<<team[i].occupy;
+        fout<<" occupying computer for"<<team[i].occupy;
       }
-      cout<<endl;
+      fout<<endl;
     }
   }
   for (int i = 0; i < 5; i++){
     if (monster[i].present==true){
-      cout<<"monster "<<i<<" "<<monster[i].name<<endl;
+      fout<<"monster "<<i<<" "<<monster[i].name<<endl;
       if (monster[i].dead==true){
-        cout<<"dead"<<endl;
+        fout<<"dead"<<endl;
       }
       else {
-        cout<<"hp: "<<monster[i].hp<<"/"<<monster[i].maxhp<<" radiation:"<<monster[i].radiation;
+        fout<<"hp: "<<monster[i].hp<<"/"<<monster[i].maxhp<<" radiation:"<<monster[i].radiation;
         if (monster[i].ignited==true){
-          cout<<" ignited, effect reamins for"<<t-monster[i].ignitedt;
+          fout<<" ignited, effect reamins for"<<t-monster[i].ignitedt;
         }
         if (monster[i].shocked==true){
-          cout<<" shocked";
+          fout<<" shocked";
         }
-        cout<<endl;
+        fout<<endl;
       }
     }
   }
